ff8dc8dd18305b8db60b3682fe684b1f5dca1c92
Lucky9113 <lakshaymenghani@gmail.com>
2023-06-03 16:39:44 +0530

    Created signup

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,41 @@
+import 'package:fintrack/signup.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MaterialApp(home: MyApp(),
+  debugShowCheckedModeBanner: false,
+      title: "Main",));
 }
 
 class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body:Container(
+  Widget build(BuildContext context) {  
+    
+    return Scaffold(
+        
+        body: Container(
           width: double.infinity,
           height: double.infinity,
           decoration: const BoxDecoration(
             image: DecorationImage(
-              image: AssetImage("images/background.jpg"), fit: BoxFit.cover),
-              ),
-          
-          child: Center(
-            child: Text(" "),
-            ) ,
-            ),
-        )
+                image: AssetImage("images/background.jpg"), fit: BoxFit.cover),
+          ),
+          child: Align(
+            alignment: Alignment(0.03, 0.325),
+            child: TextButton(
+                child: const Text(
+                  "Sign Up",
+                  style: TextStyle(color: Colors.transparent, fontSize: 28),
+                ),
+                onPressed: () {
+                  Navigator.pushReplacement(context,
+                    MaterialPageRoute(
+                      builder: (context) => sign_up(),
+                    ),
+                  );
+                }),
+          ),
+        ),
       );
-  } 
+  }
 }
